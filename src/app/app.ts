9cbf345176c51ab203c82b8bd5260f41ac016fc6
Cgabriel65 <cgabriel65@gmail.com>
2026-03-13 14:04:01 +0000

    build: create docker pipeline

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { environment } from '../environments/environment.development';
 
 
 
@@ -13,3 +14,13 @@ import { Header } from './components/header/header';
 export class App {
   protected readonly title = signal('projeto-final');
 }
+
+
+async function loadUsers() {
+  const response = await fetch(environment.apiUrl + '/users');
+  const users = await response.json();
+
+  console.log(users);
+}
+
+loadUsers();
